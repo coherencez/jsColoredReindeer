9b20b0fb5159ef333a52f3d8879951c9daf6f6b6
Thomas Swann <tkswann@mac.com>
2016-05-21 16:52:17 -0500

    changed to nested loop, and fixed undefined/repeat entries from loading to DOM

--- reindeer.js
+++ reindeer.js
@@ -1,7 +1,7 @@
 var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"],
 	  reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"],
 	  colorsLength = colors.length,
- // reinderLength for nested loops trial and error
+ // reindeerLength for nested loops trial and error
 	  reindeerLength = reindeer.length;
 
 var hohohoElement = document.getElementById("coloredReindeer");
@@ -12,17 +12,20 @@ console.log("DOM Element containing reindeer list", hohohoElement);
 // function for building up my HTML string to apply to the DOM
 var buildHTML = function buildString () {
 	var htmlString = '<ul>';
-
+	
 	for (var i = 0; i < colorsLength; i++) {
-			  // if (colors[i] !== (reindeer[i] - 1)) {
-			  	// if colors index does not match reindeer's, do nothing
-			  	// my attempt to precent 'undefined' from being rendered to the page
+		for (var j = 0; j < reindeerLength; j++) {
+			// if statement to make sure that only valid names and colors will print. 
+			// will only build a string when array indexes match
+			// also prevents inner loop from running a full cycle on every outerloop ++
+			if (i === j) {
 					htmlString += '<li>';
 					htmlString += colors[i] + ' ';
-					htmlString += reindeer[i];
+					htmlString += reindeer[j];
 					htmlString += '</li>';
-		// }
+				} 
 			}
+		}
 	htmlString += '</ul>';
 	return htmlString;
 };
